6009965ae3331c26f181ea235218590c1779e022
froisfernanda <127303960+froisfernanda@users.noreply.github.com>
2023-11-23 15:04:36 -0300

    Update index.php

--- fernanda/index.php
+++ fernanda/index.php
@@ -1,11 +1,10 @@
-<<?php
+<?php
 
-/*aqui vamos conectar 
-com o banco 
-de dados*/
+/*aqui vamos conectar com o banco de dados*/
+
 $servername = "localhost";
 //você deu nome ao banco de dados
-$database = ""; //func2c ou func2d
+$database = "func2C";
 $username = "root";
 $password = "";
 
@@ -23,24 +22,120 @@ $id = $_POST["id"];
 $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
 $botao = $_POST["botao"];
+$pesquisa = $_POST["pesquisa"];
 
-echo $id." ".$nome." ".$cpf." ".$botao;
+//aqui controla os botões
+if(empty($botao)){
 
-$sql_cadastrar = "INSERT INTO funcionarios 
+}else if($botao == "Cadastrar"){
+    $sql = "INSERT INTO funcionarios 
     (id, nome, cpf) VALUES('','$nome', '$cpf')";
+}else if($botao == "Excluir"){
+    $sql = "DELETE FROM funcionarios WHERE id = '$id'";
+}else if($botao == "Recuperar"){
+    $sql_mostra_cad = "SELECT * FROM funcionarios WHERE nome like '%$pesquisa%'";
+}else if($botao == "Alterar"){
+    $sql = "UPDATE funcionarios SET nome = '$nome', cpf = '$cpf' WHERE id = $id";
+}
 
+//aqui vou tratar erros nas operações C.E.R.A
+if(!empty($sql)){
+    if(mysqli_query($conexao, $sql)){
+        echo "Operação realizada com sucesso";
+    }else{
+        echo "Houve um erro na operação: <br />";
+        echo  mysqli_error($conexao);
+    }
+}
+
+$selecionado = $_GET["id"];
+
+if(!empty($selecionado)){
+    $sql_selecionado = "SELECT * FROM funcionarios
+                        WHERE id = $selecionado";
+    $resultado = mysqli_query($conexao, $sql_selecionado); 
+         
+    while($linha = mysqli_fetch_assoc($resultado)){
+        $id = $linha["id"];
+        $nome = $linha["nome"];
+        $cpf = $linha["cpf"];
+    }                      
+}
 ?>
 <html>
+    <head>
+        <title>Sistema C.E.R.A.</title>
+        <style>
+        #cadastrar-o-botao {
+            background-color: rgb(118, 219, 242);
+            border-radius: 5px;
+            padding: 1em;
+        }
+
+        #excluir-o-botao {
+            background-color: rgb(255, 0, 144);
+            border-radius: 5px;
+            padding: 1em;
+        }
+
+        #recuperar-o-botao {
+            background-color: rgb(55, 195, 149);
+            border-radius: 5px;
+            padding: 1em;
+        }
+
+        #alterar-o-botao {
+            background-color: rgb(183, 27, 255);
+            border-radius: 5px;
+            padding: 1em;
+        }
+    </style>
+    </head>
     <body>
+
     <form name = "func" method = "post" >
         <label>ID</label>
-        <input type ="text" name = "id" /><br />
+        <input type ="text" name = "idi" value="<?php echo $id; ?>" disabled /><br />
+        <input type ="hidden" name = "id" value="<?php echo $id; ?>"/><br />
         <label>Nome</label>
-        <input type ="text" name = "nome" /><br />
+        <input type ="text" name = "nome" value="<?php echo $nome; ?>"/><br />
         <label>CPF</label>
-        <input type ="text" name = "cpf" /><br />
-        <input type ="submit" name = "botao" value = "cadastrar" />
-        <input type ="reset" name = "botao" value = "cancelar" />
+        <input type ="text" name = "cpf" value="<?php echo $cpf; ?>"/><br />
+        <input type ="submit" name = "botao" id="cadastrar-o-botao" value = "Cadastrar" />
+        <input type ="submit" name = "botao" id="excluir-o-botao" value = "Excluir" />
+        <br />
+        <input type="text" name = "pesquisa" />
+        <input type="submit" name = "botao" id="recuperar-o-botao" value = "Recuperar" />
+        <input type="submit" name = "botao" id="alterar-o-botao" value = "Alterar" />
     </form>
+    <table>
+        <tr>
+            <td>-</td>
+            <td>ID</td>
+            <td>Nome</td>
+            <td>CPF</td>
+        </tr>
+        <?php
+        if(empty($pesquisa)){
+            $sql_mostra_cad = "SELECT * FROM funcionarios
+            ORDER BY id desc limit 0,10";
+        }
+         
+         $resultado = mysqli_query($conexao, $sql_mostra_cad); 
+         
+         while($linha = mysqli_fetch_assoc($resultado)){
+            echo "
+            <tr>
+                <td>
+                <a href='?id=".$linha["id"]."'>Selecionar</a>
+                </td>
+                <td>".$linha["id"]."</td>
+                <td>".$linha["nome"]."</td>
+                <td>".$linha["cpf"]."</td>
+            </tr>
+            ";
+         }
+        ?>
+    </table>
     </body>
 </html>
